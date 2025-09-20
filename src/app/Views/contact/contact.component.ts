c54fe8a0bece46389191dd2f24580455f401afc5
VictorMBonilla2 <lossg2234@gmail.com>
2025-09-20 16:14:42 -0500

    Estructuracion de Proyecto - Habilitacion de Router - Outlet

---
+++ src/app/Views/contact/contact.component.ts
@@ -0,0 +1,17 @@
+import { Component } from "@angular/core";
+import { RouterOutlet } from "@angular/router";
+
+
+@Component({
+  selector: 'contact',                // nombre de la etiqueta
+  standalone: true,                    // ← standalone obligatorio
+  imports: [RouterOutlet],             // Componentes importados que usaras en la vista
+  templateUrl: './contact.html', // HTML de esta vista
+  styleUrl: './contact.css'      // estilos locales
+})
+
+export class Contact{
+    title= 'Bienvenido'
+
+    
+}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { Home } from './Views/home/home.component';
+import { Contact } from './Views/contact/contact.component';
 
-export const routes: Routes = [];
+
+
+export const routes: Routes = [
+    { path: 'home', component: Home },
+    { path: 'contact', component: Contact},
+
+
+    // Ruta vacía → redirige a home
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+
+    // Ruta no encontrada → página 404
+    { path: '**', component: Home } // luego puedes crear un NotFoundComponent
+];
